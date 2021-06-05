19ba8509e194f9de373ce48448539a73def662ef
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-05 22:34:50 +0530

    compare expression

--- Arithmaticsort.sh
+++ Arithmaticsort.sh
@@ -1,6 +1,8 @@
-
-echo "Computes Different Arithmetic Expression and sorts the results"
+#compare expretions "
 
 read -p "Enter First number:" a
 read -p "Enter Second number:" b
 read -p "Enter Third number:" c
+
+Expression1=$(($a+$(($b*$c))))
+echo "Result of Expression1 is::"$Expression1
