4773ce26ddb02f111476d3935ac03cb61bfaccac
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-05 22:28:26 +0530

    three input

--- Arithmaticsort.sh
+++ Arithmaticsort.sh
@@ -1,1 +1,6 @@
-echo "sorting number"
+
+echo "Computes Different Arithmetic Expression and sorts the results"
+
+read -p "Enter First number:" a
+read -p "Enter Second number:" b
+read -p "Enter Third number:" c
